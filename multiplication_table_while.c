5250442853103eabbb67f46a6a456b1a692d60a6
seungmin <seungmincheon3@example.com>
2026-03-30 16:33:56 +0900

    구구단출력기 while문

---
+++ multiplication_table_while.c
@@ -0,0 +1,44 @@
+/*
+    * multiplication_table_while.c
+    *  학번: 20235265
+    *  이름: 천승민
+*/
+
+#include  <stdio.h>
+
+int main() {
+    int a, b,c,d;
+    printf("multiplication table for\n");
+    scanf("%d %d", &a, &b);
+    if(a>b){
+        printf("error: a > b \n");
+        return 0;
+    }
+    if (a<1 || b>9)
+    {
+        printf("error: a < 1 || b > 9 \n");
+        return 0;
+    }
+    scanf("%d %d", &c, &d);
+    if(c>d){
+        printf("error: c > d \n");
+        return 0;
+    }
+    if (c<1 || d>12)
+    {
+        printf("error: c < 1 || d > 12 \n");
+        return 0;
+    }
+
+    int i = a;
+    while(i <= b){
+        int j = c;
+        while(j <= d){
+            printf("%d * %d = %d\n", i, j, i*j);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+        return 0;
+}
